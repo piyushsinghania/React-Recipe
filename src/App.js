7aa558c69176f01e0d2162f98ada37d0bc4e2162
Piyush Singhania <piyushsinghania28@gmail.com>
2022-01-23 00:01:07 +0530

    Setup done for React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Styles
+import './App.css';
+
+// Page Components
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
